efcc918277a3f2d60ea2d7d51991bc8cd2c64f72
frh44n <107492548+frh44n@users.noreply.github.com>
2024-07-28 03:29:41 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
-# new_features.py
-from telegram import Update, Bot
+# test.py
+from telegram import Update
 from telegram.ext import CommandHandler, MessageHandler, Filters, CallbackContext
 
 ADMIN_USER_ID = 7090230279
-TARGET_USER_ID = None  # Placeholder for the user ID to chat with
+TARGET_USER_ID = None
 
-def start(update: Update, context: CallbackContext):
+def start_admin_chat(update: Update, context: CallbackContext):
     if update.effective_user.id == ADMIN_USER_ID:
         update.message.reply_text('Hello Admin! Use /chat <user_id> to start chatting with a user.')
     else:
@@ -16,25 +16,26 @@ def chat(update: Update, context: CallbackContext):
     if update.effective_user.id == ADMIN_USER_ID:
         try:
             TARGET_USER_ID = int(context.args[0])
+            initial_message = ' '.join(context.args[1:])  # Get the initial message
             update.message.reply_text(f'Started chat with user {TARGET_USER_ID}.')
+            if initial_message:
+                context.bot.send_message(chat_id=TARGET_USER_ID, text=initial_message)
         except (IndexError, ValueError):
-            update.message.reply_text('Usage: /chat <user_id>')
+            update.message.reply_text('Usage: /chat <user_id> <message>')
     else:
         update.message.reply_text('You are not authorized to use this command.')
 
 def forward_message(update: Update, context: CallbackContext):
     global TARGET_USER_ID
-    if update.effective_user.id == ADMIN_USER_ID:
-        if TARGET_USER_ID:
-            context.bot.send_message(chat_id=TARGET_USER_ID, text=update.message.text)
-        else:
-            update.message.reply_text('No user ID specified. Use /chat <user_id> to set a user ID.')
-    elif update.effective_user.id == TARGET_USER_ID:
-        context.bot.send_message(chat_id=ADMIN_USER_ID, text=update.message.text)
-    else:
-        update.message.reply_text('You are not authorized to use this bot.')
+    user_id = update.effective_user.id
+    message_text = update.message.text
+
+    if user_id == ADMIN_USER_ID and TARGET_USER_ID:
+        context.bot.send_message(chat_id=TARGET_USER_ID, text=message_text)
+    elif user_id == TARGET_USER_ID:
+        context.bot.send_message(chat_id=ADMIN_USER_ID, text=message_text)
 
 def register_new_handlers(dispatcher):
-    dispatcher.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(CommandHandler('start_chat', start_admin_chat))
     dispatcher.add_handler(CommandHandler('chat', chat, pass_args=True))
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, forward_message))
