02b4304ffbd146133e8b42a888d41ee72d20a27b
frh44n <107492548+frh44n@users.noreply.github.com>
2024-07-28 06:02:24 +0530

    Update withdraw.py

--- withdraw.py
+++ withdraw.py
@@ -6,7 +6,14 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 def withdraw_command(update: Update, context: CallbackContext):
-    update.message.reply_text('Contact "@ludo_king_help", to withdraw money 🤑.')
+    user_id = update.message.from_user.id
+    update.message.reply_text(
+        f"👉Withdrawal Request👈\n"
+        f"Your user id: {user_id}\n"
+        f"Amount :-\n"
+        f"UPI id :-\n\n"
+        f"Copy this message and enter details, send to @ludo_king_help."
+    )
 
 def register_withdraw_handlers(dispatcher):
     dispatcher.add_handler(CommandHandler('Withdraw', withdraw_command))
